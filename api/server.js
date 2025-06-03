67f6d3e1695cef2d0831fe02485007003cbc06e9
Cookiescooker <fhhdhysd@gmail.com>
2025-06-03 19:09:34 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `${ENV_VARS.API_ROUTE}`,
+    origin: `${ENV_VARS.CLIENT_URL}`,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
